5be3c8c837e2274f9839732ba9b4bd530fc2bc5c
DVD <dvd@nothey.com>
2014-07-24 16:18:45 -0700

    Revert if needed

--- js/app/com/dadc/lithium/view/widgets/SubtitleWidget.js
+++ js/app/com/dadc/lithium/view/widgets/SubtitleWidget.js
@@ -50,69 +50,88 @@ var SubtitleWidget = function( SubtitlesObj ) {
             m_subtitle_container.removeChildAt( 0 );
         }
     }
-    this.displaySubtitleBetween = function( start_seconds, end_seconds ){
-        Logger.log("displaySubtitleBetween called");
-        if( !m_subtitles_obj ){
-            Logger.log("there is no subtitle object, returning");
-            return;
-        }
+    //No longer used
+    // this.displaySubtitleBetween = function( start_seconds, end_seconds ){
+    //     Logger.log("displaySubtitleBetween called");
+    //     if( !m_subtitles_obj ){
+    //         Logger.log("there is no subtitle object, returning");
+    //         return;
+    //     }
 
-        var subtitle = m_subtitles_obj.getSubtitleBetween( parseInt( start_seconds ), parseInt( end_seconds ) );
+    //     var subtitle = m_subtitles_obj.getSubtitleBetween( parseInt( start_seconds ), parseInt( end_seconds ) );
 
-        if( subtitle != m_last_subtitle ){
-            Logger.log("subtitle != m_last_subtitle");
-            this.removeSubtitleFromScreen();
+    //     if( subtitle != m_last_subtitle ){
+    //         Logger.log("subtitle != m_last_subtitle");
+    //         //this.removeSubtitleFromScreen();
 
-            if ( subtitle ){
-                Logger.log("subtitle is true. will add child");
-                m_subtitle_container.addChild( getTextContainer( subtitle.getText() ) );
-            }else{
-                Logger.log("subtitle is not true, will not add");
-            }
-        }else{
-            Logger.log("subtitle != m_last_subtitle");
-        }
-    }
+    //         if ( subtitle ){
+    //             Logger.log("subtitle is true. will add child");
+    //             //m_subtitle_container.addChild( getTextContainer( subtitle.getText() ) );
+    //             var sst = subtitle.getText()
+    //             tblock.text = getTextContainer( sst)
+    //         }else{
+    //             Logger.log("subtitle is not true, will not add");
+    //         }
+    //     }else{
+    //         Logger.log("subtitle != m_last_subtitle");
+    //     }
+    // }
     this.displaySubtitleLine = function( SubtitleLineObj ){
-        this.removeSubtitleFromScreen();
-
+        //this.removeSubtitleFromScreen();
         if ( SubtitleLineObj ){
             Logger.log("SubtitleLineObj exists, will add");
-            m_subtitle_container.addChild( getTextContainer( SubtitleLineObj.getText() ) );
+            //m_subtitle_container.addChild( getTextContainer( SubtitleLineObj.getText() ) );
+            var sst = SubtitleLineObj.getText()
+            var formattedText = getTextContainer(sst)
+            tblock.text = formattedText
+            shadowBlock.text = formattedText
+            //tblock.text = sst
+            //shadowBlock.text = sst
         }else{
             Logger.log("SubtitleLineObj does not exist, will not add");
         }
     }
+    var tblock;
+    var shadowBlock;
     function initWidget(){
+        tblock = engine.createTextBlock( " ", FontLibraryInstance.getFont_SUBTITLE(), 1700 );
+        shadowBlock = engine.createTextBlock( " ", FontLibraryInstance.getFont_SUBTITLESHADOW(), 1700 );
 
+        tblock.x = 0
+        tblock.y = 0
+        shadowBlock.x = tblock.x+1
+        shadowBlock.y = tblock.y+1
+        m_subtitle_container.addChild(shadowBlock)
+        m_subtitle_container.addChild(tblock)
+        // tblock.x = 1920 / 2 - tblock.naturalWidth / 2;
+        // shadowBlock.x = tblock.x+1
+        // tblock.y = 0//  -( tblock.naturalHeight ) * 50;
+        // shadowBlock.y = tblock.y+1
     }
     function getTextContainer( message ){
         Logger.log("getTextContainer for message: " + message);
-        var tblock;
-        var shadowBlock;
         var messages = message.split( TTMLSubtitle.CONFIG.BREAK_LINES );
-        var container = engine.createContainer();
+        //var container = engine.createContainer();
 
-    	// DAN: engine.createTextBlock failed, surrounding with a try-catch in hopes to "skip" the offending subtitle
-    	try
-    	{
-    	    for( var i = 0; i < messages.length; i++ ){
-        		var tmp_container = engine.createContainer();
-        		//var shader = ShaderCreatorInstance.createGlowShader( 4, RGBLibraryInstance.getBLACK( 1 ), RGBLibraryInstance.getWHITE( 1 ) );
+        // DAN: engine.createTextBlock failed, surrounding with a try-catch in hopes to "skip" the offending subtitle
+        try
+        {
+            var sub="";
+            for( var i = 0; i < messages.length; i++ ){
+            //  //var tmp_container = engine.createContainer();
+            //  //var shader = ShaderCreatorInstance.createGlowShader( 4, RGBLibraryInstance.getBLACK( 1 ), RGBLibraryInstance.getWHITE( 1 ) );
 
-        		tblock = engine.createTextBlock( messages[ i ], FontLibraryInstance.getFont_SUBTITLE(), 1700 );
-        	//            tblock.shader = shader;
-        		// disabled the glow shader
-                shadowBlock = engine.createTextBlock( messages[ i ], FontLibraryInstance.getFont_SUBTITLESHADOW(), 1700 );
-                tmp_container.addChild( shadowBlock );
-                tmp_container.addChild( tblock );
-        		tblock.x = 1920 / 2 - tblock.naturalWidth / 2;
-        		tblock.y = -( ( messages.length - 1 ) - i ) * 50;
-                shadowBlock.x = tblock.x+1
-                shadowBlock.y = tblock.y+1
+            //  //tblock = engine.createTextBlock( messages[ i ], FontLibraryInstance.getFont_SUBTITLE(), 1700 );
+            // //            tblock.shader = shader;
+            //  // disabled the glow shader
 
-        		container.addChild( tmp_container );
-    	    }
+            //  //tmp_container.addChild( tblock );
+                sub += messages[i]+"\n"
+
+         //        //container.addChild( tmp_container );
+         //        //return message
+             }
+            return sub
     	}
     	catch(e)
     	{
@@ -120,6 +139,6 @@ var SubtitleWidget = function( SubtitlesObj ) {
     	}
 
             Logger.log("returning container");
-            return container;
+            return "";
         }
 };
